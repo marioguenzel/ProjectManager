e46952a424aeab352dfae10b840dc67a159db125
Mario <mario.guenzel@tu-dortmund.de>
2024-10-20 14:37:15 +0200

    package data

--- setup.py
+++ setup.py
@@ -11,6 +11,10 @@ setup(
     version='0.1.0',  # Version of your package
     packages=find_packages(),  # Automatically find packages in the directory
     install_requires=load_requirements(),  # List any dependencies here
+    include_package_data=True,  # This is important to include non-code files.
+    package_data={
+        'project-manager': ['template_projects.yaml'],
+    },
     description='A simple project manager for resources like GIT, SVN, LINKS, etc.',
     author='Mario Guenzel',
     url='https://github.com/marioguenzel/ProjectManager',
